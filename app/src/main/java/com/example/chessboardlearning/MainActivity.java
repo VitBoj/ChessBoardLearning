d4c6c5a5748bb43f5d81956669a4f97b4fc8527d
vitbozh <vitboj22@gmail.com>
2021-02-20 12:23:08 +0200

    ...

--- app/src/main/java/com/example/chessboardlearning/MainActivity.java
+++ app/src/main/java/com/example/chessboardlearning/MainActivity.java
@@ -1,130 +1,108 @@
 package com.example.chessboardlearning;
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+
+import com.example.chessboardlearning.databinding.ActivityMainBinding;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-////////////////////////////////////////////////////////////////////////////////////////////////////
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView viewAsk,answer;
-    Button buttonWhite,buttonBlack,refresh;
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        viewAsk=findViewById(R.id.askAndResultView);
-        answer=findViewById(R.id.answer);
-        buttonBlack=findViewById(R.id.buttonBlack);
-        buttonWhite=findViewById(R.id.buttonWhite);
-        refresh=findViewById(R.id.refresh);
-        buttonBlack.setOnClickListener(this);
-        buttonWhite.setOnClickListener(this);
-        refresh.setOnClickListener(this);
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.buttonBlack.setOnClickListener(this);
+        binding.buttonWhite.setOnClickListener(this);
+        binding.refresh.setOnClickListener(this);
         FillMap();
         ShowAsk(FillMap());
     }
-////////////////////////////////////////////////////////
-    public Map FillMap(){
-        Map<String, String> hashMap = new HashMap<String, String>();
-        for(int i=1;i<9;i++){
-            if(isEven(i)==true){
-                hashMap.put("A"+convToStr(i),"White");}
-            else hashMap.put("A"+convToStr(i),"Black");
+
+    public Map FillMap() {
+        Map<String, String> hashMap = new HashMap<>();
+        char[] firstLettersSet = {'A', 'C', 'E', 'G'};
+        char[] secondLettersSet = {'B', 'D', 'F', 'H'};
+        for (char letter : firstLettersSet) {
+            fillFirstSet(hashMap, letter);
         }
-        for(int i=1;i<9;i++){
-            if(isEven(i)==true){
-                hashMap.put("B"+convToStr(i),"Black");}
-            else hashMap.put("B"+convToStr(i),"White");
-        }for(int i=1;i<9;i++){
-            if(isEven(i)==true){
-                hashMap.put("C"+convToStr(i),"White");}
-            else hashMap.put("C"+convToStr(i),"Black");
-        }for(int i=1;i<9;i++){
-            if(isEven(i)==true){
-                hashMap.put("D"+convToStr(i),"Black");}
-            else hashMap.put("D"+convToStr(i),"White");
-        }for(int i=1;i<9;i++){
-            if(isEven(i)==true){
-                hashMap.put("E"+convToStr(i),"White");}
-            else hashMap.put("E"+convToStr(i),"Black");
-        }for(int i=1;i<9;i++){
-            if(isEven(i)==true){
-                hashMap.put("F"+convToStr(i),"Black");}
-            else hashMap.put("F"+convToStr(i),"White");
-        }for(int i=1;i<9;i++){
-            if(isEven(i)==true){
-                hashMap.put("G"+convToStr(i),"White");}
-            else hashMap.put("G"+convToStr(i),"Black");
-        }
-        for(int i=1;i<9;i++){
-            if(isEven(i)==true){
-                hashMap.put("H"+convToStr(i),"Black");}
-            else hashMap.put("H"+convToStr(i),"White");
+        for (char letter : secondLettersSet) {
+            fillSecondSet(hashMap, letter);
         }
         return hashMap;
     }
-/////////////////////////////////////////////////////////
-    public boolean isEven(int n){
-        if(n % 2 == 0) {
-            //Четное
-            return  true;
+
+    public void fillFirstSet(Map<String, String> map, char setLetter) {
+        for (int i = 1; i < 9; i++) {
+            if (isEven(i) == true) {
+                map.put(setLetter + String.valueOf(i), "Black");
+            } else map.put(setLetter + String.valueOf(i), "White");
+        }
+    }
+
+    public void fillSecondSet(Map<String, String> map, char setLetter) {
+        for (int i = 1; i < 9; i++) {
+            if (isEven(i) == true) {
+                map.put(setLetter + String.valueOf(i), "White");
+            } else map.put(setLetter + String.valueOf(i), "Black");
+        }
+    }
+
+    public boolean isEven(int n) {
+        if (n % 2 == 0) {
+            return true;
         } else {
             return false;
         }
     }
-/////////////////////////////////////
-    public  String convToStr(int i){
-        String s=String.valueOf(i);
-        return s;
-    }
-/////////////////////////////////////////////////////////////////////
-    public void ShowAsk(Map hashMap){
-        List<String> valuesList = new ArrayList<String>(hashMap.keySet());
+
+    public void ShowAsk(Map hashMap) {
+        List<String> valuesList = new ArrayList<>(hashMap.keySet());
         int randomIndex = new Random().nextInt(valuesList.size());
         String randomValue = valuesList.get(randomIndex);
-        viewAsk.setText(randomValue);
-        answer.setText(" ");
-
-
-
+        binding.askAndResultView.setText(randomValue);
+        binding.answer.setText(" ");
     }
-///////////////////////////////////////////////////////////////////////
+
     public void CheckIfCorrect(String color) {
-        String key=viewAsk.getText().toString();
+        String key = binding.askAndResultView.getText().toString();
         String value = FillMap().get(key).toString();
-        if(value.equals(color)){
-            answer.setTextColor(getResources().getColor(R.color.green));
-            answer.setText("Верно");
+        if (value.equals(color)) {
+            binding.answer.setTextColor(getResources().getColor(R.color.green));
+            binding.answer.setText("Верно");
+        } else {
+            binding.answer.setTextColor(getResources().getColor(R.color.red));
+            binding.answer.setText("Не верно");
         }
-        else {
-            answer.setTextColor(getResources().getColor(R.color.red));
-            answer.setText("Не верно");
-        }
-////////////////////////////////////////
     }
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case (R.id.buttonBlack):
-                String color="Black";
+                String color = "Black";
                 CheckIfCorrect(color);
                 break;
             case (R.id.buttonWhite):
-                String color2="White";
+                String color2 = "White";
                 CheckIfCorrect(color2);
                 break;
-            case(R.id.refresh):
+            case (R.id.refresh):
                 ShowAsk(FillMap());
-           default:
-               break;
+            default:
+                break;
         }
     }
-////////////////////////////////////////////////////////////////////////////////////////////////////
 }
